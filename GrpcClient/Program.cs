a5ecebe57617c815c75836874c0e7928d3e34bb2
cwiersema24 <coraw24@gmail.com>
2020-12-09 17:17:21 -0500

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Calling a gRpc Service!");
+            Console.WriteLine("hit enter to call the service...");
+            Console.ReadLine();
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            Console.Write("Enter Your Name: ");
+            var name = Console.ReadLine();
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+            Console.WriteLine($"This service said {reply.Message}");
+
+
+
+            Console.Write("Where do you want to go today?");
+            var address = Console.ReadLine();
+            var client2 = new TurnByTurn.TurnByTurnClient(channel);
+            var streamingResponse = client2.StartGuidance(new GuidanceRequest { Address = address });
+            await foreach(var step in streamingResponse.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"Turn {step.Direction} at {step.Road}");
+            }
+
+            Console.WriteLine("You have arrived at " + address);
+        }
+    }
+}
---
+++ GrpcService/Services/TurnByTurnService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcService.Services
+{
+    public class TurnByTurnService:TurnByTurn.TurnByTurnBase
+    {
+        public override async Task StartGuidance(GuidanceRequest request, IServerStreamWriter<Step> responseStream, ServerCallContext context)
+        {
+            var steps = new List<Step> 
+            { 
+                new Step { Direction = "Left", Road = "Morse" },
+                new Step { Direction = "Left", Road = "Hill" },
+                new Step { Direction = "Right", Road = "Broadway" },
+            };
+
+            foreach (var step in steps)
+            {
+                await Task.Delay(new Random().Next(2000, 5000));
+                await responseStream.WriteAsync(step);
+            }
+            
+        }
+    }
+}
